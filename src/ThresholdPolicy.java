5fbfc188630009af4c2f94f8ed23d34d05907d9b
brendanblack <brendan.black15@gmail.com>
2025-03-25 17:45:19 -0400

    Gave ThresholdPolicy a StandardFarkleScorer

--- src/ThresholdPolicy.java
+++ src/ThresholdPolicy.java
@@ -2,9 +2,11 @@ import java.util.*;
 
 public class ThresholdPolicy implements Policy {
     private int threshold;
+    private FarkleScorer scorer;
 
     public ThresholdPolicy(int threshold) {
         this.threshold = threshold;
+        this.scorer = new StandardFarkleScorer();
     }
 
     public boolean shouldRollAgain(int runningTotal, List<Integer> remainingDice) {
@@ -14,4 +16,6 @@ public class ThresholdPolicy implements Policy {
     public String getName() {
         return "Threshold(" + threshold + ")";
     }
+
+    public FarkleScorer getFarkleScorer() { return scorer; }
 }
