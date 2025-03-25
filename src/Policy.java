f8e65a85e1987c65b50f07295597312cad03402e
brendanblack <brendan.black15@gmail.com>
2025-03-25 17:44:51 -0400

    Added getFarkleScorer() to Policy interface so policies have to use a certain scorer

--- src/Policy.java
+++ src/Policy.java
@@ -3,4 +3,5 @@ import java.util.*;
 public interface Policy {
     boolean shouldRollAgain(int runningTotal, List<Integer> remainingDice);
     String getName();
+    FarkleScorer getFarkleScorer();
 }
